5dad39121ca5331f2189a9218b54a72c0ce59e0a
Jean Paull <jeanfdq@gmail.com>
2019-02-19 09:54:10 -0300

    Base64

--- app/src/main/java/com/example/whatsapp/helper/Base64EncodeCode.java
+++ app/src/main/java/com/example/whatsapp/helper/Base64EncodeCode.java
@@ -2,11 +2,11 @@ package com.example.whatsapp.helper;
 
 import android.util.Base64;
 
-//para cript em base 64code
+
 public class Base64EncodeCode {
 
 	public static String Encode64(String text){
-		return Base64.encodeToString(text.getBytes(), Base64.DEFAULT).replaceAll("(\\n|\\r)","");
+		return Base64.encodeToString(text.getBytes(), Base64.DEFAULT).replaceAll("(\\n|\\r)",""); //retira o enter da string
 	}
 
 	public static String Decode64(String text){
